75d73b9494aef5e47299499dfb4977e3137afde8
JeanMieses <araujo0275@gmail.com>
2021-11-15 16:23:20 -0500

    fixing bug

--- seeds/seeding.js
+++ seeds/seeding.js
@@ -2,6 +2,8 @@ const Feedback = require('./../models/feedback');
 const data = require('./data.json');
 const mongoose = require('mongoose');
 
+//
+
 mongoose.connect('mongodb+srv://JeanProductFeedback:s7tex3GIscrXqQ2a@cluster0.v0j1k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {
   console.log('connected to mongoose');
